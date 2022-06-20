be73a924064ecd54a01f55cef55c56c19dbc54d1
IxtiyorSadullayev <sadullayevixtiyor@gmail.com>
2022-06-20 15:44:17 +0500

    initial

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-let uzgrauvchi;
+// Barcha ishlarni qilib bo`lgandan keyin 
+// git add .
+// git commit -m "Bu yerga habar yozasan"
+// git push
+
+const {Telegraf} = require('telegraf')
+const token = ""
+const bot = new Telegraf(token)
+
+
+
+
+bot.start(msg =>{
+    msg.reply('Hush kelibsiz.')
+})
+
+
+
+
+bot.launch()
+
